caf3b2c672ebdd37149a8cea4f93c8507be8f039
chodi <chanerandio@gmail.com>
2017-11-02 11:33:44 +0800

    added logs for auth

--- server/middleware/auth-check.js
+++ server/middleware/auth-check.js
@@ -3,11 +3,15 @@ const User = require('mongoose').model('User');
 // const Auth = require('../modules/Auth');
 const isDev = process.env.NODE_ENV !== 'production';
 const secret = isDev ? require('../../SECRET').secretkey : '';
-const thisisacomplexkeyword = process.env.COMPLEX_HASH_LETTERS;
+const thisisacomplexkeyword = isDev ? secret : process.env.COMPLEX_HASH_LETTERS;
 /**
  *  The Auth Checker middleware function.
  */
 module.exports = (req, res, next) => {
+  console.log('############################')
+  console.log('### IM HERE at auth-check')
+  console.log('### URL', req.url)
+  console.log('############################')
   if (req.url === '/logout') {
     return next();
   }
@@ -18,13 +22,22 @@ module.exports = (req, res, next) => {
   if (req.url === '/signup' || req.url === '/auth/signup' || (req.url === '/login' && (req.method === 'POST' || req.method === 'GET')) || (req.url === '/auth/login' && req.method === 'POST')) {
     return next();
   }
+  console.log('############################')
+  console.log('tokenExists', tokenExists)
+  console.log('############################')
   const tokenExists = req.headers.authorization || req.cookies.userToken;
   if (!tokenExists) {
+    console.log('############################')
+    console.log('ID DONT HAVE TOKEN at url', req.url)
+    console.log('############################')
     return res.render('login', { message: 'token not exist' });
   }
   const token = req.cookies.userToken || req.headers.authorization.split(' ')[1];
   // decode the token using a secret key-phrase
 
+  console.log('############################')
+  console.log('token ', tokenExists,'@ URL', req.url)
+  console.log('############################')
   return jwt.verify(token, thisisacomplexkeyword, (err, decoded) => {
     // the 401 code is for unauthorized status
     if (err) { return res.status(401).end(); }
--- server/routes/index.js
+++ server/routes/index.js
@@ -12,6 +12,10 @@ router.get('/', (req, res) => {
 
 // Sign up Page
 router.get('/signup', (req, res) => {
+  console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+  console.log('NODE.ENV', process.env.NODE_ENV)
+  console.log('### IM HERE at RENDER signup')
+  console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
   res.render('signup');
 });
 
