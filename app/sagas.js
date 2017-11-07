ef1416ed4f63bb94ad0967617f7644df0a50deb7
chodi <chanerandio@gmail.com>
2017-11-07 20:16:56 +0800

    remove unnecessary saga

--- app/sagas.js
+++ app/sagas.js
@@ -1,9 +1,7 @@
 import home from 'containers/Home/saga';
 import todo from 'pages/TODO/sagas';
-import navigationList from 'containers/NavigationList/sagas';
 
 export default [
   ...home,
   ...todo,
-  ...navigationList,
 ];
