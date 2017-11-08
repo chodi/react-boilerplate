a73c2585e06136c474216061edba1ca1afd8c5aa
chodi <chanerandio@gmail.com>
2017-11-08 23:33:17 +0800

    added splash screen

---
+++ app/components/SplashScreen/messages.js
@@ -0,0 +1,13 @@
+/*
+ * SplashScreen Messages
+ *
+ * This contains all the text for the SplashScreen component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  loadingText: {
+    id: 'app.components.SplashScreen.loadingText',
+    defaultMessage: 'Initializing...',
+  },
+});
